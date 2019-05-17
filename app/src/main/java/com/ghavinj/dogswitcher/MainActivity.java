d79acdfaaa40c911fe3cc409de3b1cd5b8303b5b
GhavinJ <ghavinj@icloud.com>
2019-05-17 19:46:01 -0400

    setImageResource to change when button is clicked.

--- app/src/main/java/com/ghavinj/dogswitcher/MainActivity.java
+++ app/src/main/java/com/ghavinj/dogswitcher/MainActivity.java
@@ -1,14 +1,19 @@
 package com.ghavinj.dogswitcher;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     public void switchDogs(View view){
+        ImageView dogImageView = (ImageView) findViewById(R.id.dogImageView);
 
+        dogImageView.setImageResource(R.drawable.dog2);
     }
 
     @Override
